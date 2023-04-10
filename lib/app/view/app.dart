190c81fa050e634a65376745ab62fdcd0b08c289
alex <ialexies@gmail.com>
2023-04-11 07:43:44 +0800

    Initialize GetX State Management

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,22 +1,16 @@
-import 'package:doc_prescriptions/counter/counter.dart';
-import 'package:doc_prescriptions/l10n/l10n.dart';
+import 'package:doc_prescriptions/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
-        ),
-      ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+    return GetMaterialApp(
+      title: 'Application',
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
     );
   }
 }
