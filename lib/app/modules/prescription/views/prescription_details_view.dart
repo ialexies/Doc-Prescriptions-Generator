19363ec3f11cc9273303bda66f7e7023c3b96f3c
alex <ialexies@gmail.com>
2023-04-12 19:38:56 +0800

    Added style to Prescriptions

--- lib/app/modules/prescription/views/prescription_details_view.dart
+++ lib/app/modules/prescription/views/prescription_details_view.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: cast_nullable_to_non_nullable
+// ignore_for_file: cast_nullable_to_non_nullable, require_trailing_commas
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:doc_prescriptions/app/modules/prescription/controllers/prescription_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:prescription_repository/prescription_repository.dart';
 
@@ -16,55 +17,85 @@ class PrescriptionDetailsView extends GetView<PrescriptionController> {
         title: const Text('Doc Prescriptions'),
         centerTitle: true,
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          // mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            StreamBuilder<DocumentSnapshot>(
-              stream:
-                  controller.prescriptionRepository.prescriptionColDetails(),
-              builder: (
-                BuildContext context,
-                AsyncSnapshot<DocumentSnapshot> snapshot,
-              ) {
-                if (snapshot.hasError) {
-                  return const Text('Something went wrong');
-                }
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: controller.prescriptionRepository.prescriptionColDetails(),
+        builder: (
+          BuildContext context,
+          AsyncSnapshot<DocumentSnapshot> snapshot,
+        ) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
 
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Expanded(
-                    child: Center(child: CircularProgressIndicator.adaptive()),
-                  );
-                }
-                if (snapshot.data == null) return const SizedBox.shrink();
-                if (snapshot.data!.data() == null) {
-                  return const SizedBox.shrink();
-                }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Expanded(
+              child: Center(child: CircularProgressIndicator.adaptive()),
+            );
+          }
+          if (snapshot.data == null) return const SizedBox.shrink();
+          if (snapshot.data!.data() == null) {
+            return const SizedBox.shrink();
+          }
 
-                final data = snapshot.data!.data() as Map<String, dynamic>;
-                final client = ClientModel.fromMap(data);
-                return Builder(
-                  builder: (context) {
-                    return Column(
-                      children: [
-                        Text(
-                          '${client.clientFirstName} ${client.clientLastName}',
-                        ),
-                        Column(
+          final data = snapshot.data!.data() as Map<String, dynamic>;
+          final client = ClientModel.fromMap(data);
+          return Builder(
+            builder: (context) {
+              return SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const CircleAvatar(
+                      radius: 80,
+                      backgroundImage: NetworkImage(
+                          'https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg'),
+                    ),
+                    Text(
+                      '${client.clientFirstName} ${client.clientLastName}',
+                      style: TextStyle(
+                        fontSize: 60.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    // const SizedBox(
+                    //   height: 20,
+                    // ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.amber[50],
+                            border: Border.all(
+                              color: Colors.amber.shade800,
+                              width: .5,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                        margin: const EdgeInsets.all(20),
+                        child: Column(
                           children: (client.prescription ?? [])
-                              .map((e) => Text(e.drugName ?? ''))
+                              .map(
+                                (e) => ListTile(
+                                  leading: Text(e.dossage ?? ''),
+                                  title: Text(e.drugName ?? ''),
+                                  subtitle: Text(e.details ?? ''),
+                                  trailing: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.edit),
+                                  ),
+                                ),
+                              )
                               .toList(),
                         ),
-                      ],
-                    );
-                  },
-                );
-              },
-            )
-          ],
-        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
