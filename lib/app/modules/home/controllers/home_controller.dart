b9c639d530dcb6894e75c5a70e8c84ccd9474871
alex <ialexies@gmail.com>
2023-04-14 23:20:10 +0800

    Add video in about page

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,12 +2,27 @@
 
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
 
 class HomeController extends GetxController {
   final authRepository = Get.find<AuthRepository>();
   final count = 0.obs;
   void increment() => count.value++;
   Rx<int> selectedIndex = 0.obs;
+
+  late VideoPlayerController vidController;
+
+  @override
+  void onInit() {
+    super.onInit();
+    vidController = VideoPlayerController.asset(
+      'assets/images/about_bkg.mp4',
+    )..initialize().then((_) {
+        vidController
+          ..play()
+          ..setLooping(true);
+      });
+  }
 
   void onItemTapped(int index) {
     selectedIndex.value = index;
