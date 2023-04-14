faa5b89357ee0429b6a08d1facc33d6dd3255b86
alex <ialexies@gmail.com>
2023-04-14 19:03:16 +0800

    Update about page

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -9,48 +9,69 @@ class HomeView extends GetView<HomeController> {
   HomeView({super.key});
   final PageController pageController = PageController();
 
-  static const List<Widget> homeViews = <Widget>[
+  static List<Widget> homeViews = <Widget>[
     ClientView(),
-    Text(
-      'Index 1: Business',
-    ),
-    Text(
-      'Index 2: School',
+    ConstrainedBox(
+      constraints:
+          BoxConstraints(minWidth: double.infinity, minHeight: double.infinity),
+      child: Container(
+        color: Colors.grey.shade500,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text(
+                'FLUTTER APP (Firebase + Flutter)',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Created By Alexies Iglesia',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                '(Firbase Auth, FlutterFire, Very Good Venture, GetX, )',
+              ),
+            ],
+          ),
+        ),
+      ),
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Doc Prescriptions'),
-        centerTitle: true,
+    return Obx(
+      () => Scaffold(
+        appBar: AppBar(
+          title: const Text('Doc Prescriptions'),
+          centerTitle: true,
+        ),
+        drawer: const DPDrawer(),
+        floatingActionButton: controller.selectedIndex.value != 0
+            ? null
+            : FloatingActionButton(
+                onPressed: () => Get.toNamed(Routes.prescriptionAdd),
+                child: const Icon(Icons.add),
+              ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.medical_information),
+              label: 'Clients',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info),
+              label: 'About',
+            ),
+          ],
+          currentIndex: controller.selectedIndex.value,
+          selectedItemColor: Colors.amber[800],
+          onTap: controller.onItemTapped,
+        ),
+        body: Obx(() => homeViews.elementAt(controller.selectedIndex.value)),
       ),
-      drawer: const DPDrawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Get.toNamed(Routes.prescriptionAdd),
-        child: const Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.medical_information),
-            label: 'Clients',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: controller.selectedIndex.value,
-        selectedItemColor: Colors.amber[800],
-        onTap: controller.onItemTapped,
-      ),
-      body: Obx(() => homeViews.elementAt(controller.selectedIndex.value)),
     );
   }
 }
