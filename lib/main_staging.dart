5cc25b4247b0504bb4b5dd281e957ccf2bc5d0b9
alex <ialexies@gmail.com>
2023-04-16 22:37:19 +0800

    Setup Prod and Staging environment from dev environment

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,6 +1,33 @@
+import 'dart:developer';
 import 'package:doc_prescriptions/app/app.dart';
 import 'package:doc_prescriptions/bootstrap.dart';
+import 'package:doc_prescriptions/firebase_options.dart';
+import 'package:doc_prescriptions/initialize_controllers.dart';
+import 'package:doc_prescriptions/initialize_providers.dart';
+import 'package:doc_prescriptions/initialize_repositories.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
-  bootstrap(() => const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(
+    name: 'docPrescriptions',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await initializeProviders();
+  await initializeRepositories();
+  await initializeControllers();
+
+  await bootstrap(() => const App());
+}
+
+Future<void> initServices() async {
+  log('starting Servicess....');
+
+  // Add my services here later
+
+  log('All Services started');
 }
