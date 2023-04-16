6c6e47dac31505bcff5736edec3133da1dd8b8c2
alex <ialexies@gmail.com>
2023-04-16 22:30:44 +0800

    Fix lint errors

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -8,7 +8,6 @@ import 'package:doc_prescriptions/initialize_repositories.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get_storage/get_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
